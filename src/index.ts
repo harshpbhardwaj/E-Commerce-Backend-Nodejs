812f122b65ed134eb7438938a716c633fc206307
Harsh <46666678+harshpbhardwaj@users.noreply.github.com>
2024-03-17 20:36:00 +0530

    database connected harshpbhardwaj@protonmail.com

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose"
 
 const app = express();
 
@@ -14,4 +15,16 @@ app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
+const mongo_uri = "mongodb+srv://harshpbhardwaj:6xBVKGQfFG9QABp9@ecom.av3axnl.mongodb.net/?retryWrites=true&w=majority&appName=ECom";
+mongoose.Promise = Promise;
+mongoose.connect(mongo_uri);
+mongoose.connection.on( 'error', ( error: Error)=> console.log(error))
+
+
+// const server = http.createServer(app);
 const server = http.createServer(app);
+const port = 5050;
+server.listen(port,() => {
+    const address = server.address();
+    console.log('Server running on http://localhost:'+String(port)+' and http://127.0.0.1:'+String(port));
+});
